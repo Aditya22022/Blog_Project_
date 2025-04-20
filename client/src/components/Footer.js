c928682448b8c154200d0fbb5c2d1aaa1c701872
Aditya22022 <adityatripathi9451@gmail.com>
2025-04-20 12:05:52 +0530

    Initial commit

---
+++ client/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import { Link } from 'react-router-dom'
+
+// eslint-disable-next-line func-style
+export default function Footer() {
+    return (
+        <footer className="blog-footer">
+            <p>
+                Blog template built for{' '}
+                <a href="https://getbootstrap.com/">Bootstrap</a> by{' '}
+                <a href="https://twitter.com/mdo">@mdo</a>.
+            </p>
+            <p>{/* <Link to="/">Back to top</Link> */}</p>
+        </footer>
+    )
+}
